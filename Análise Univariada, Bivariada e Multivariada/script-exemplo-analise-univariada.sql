efe054f75c9038ce82c9f840fd52c2e87e8d1126
Phelipe Sempreboni <phelipe.utsempreboni@outlook.com>
2023-08-22 23:41:55 -0300

    Atualização de arquivo.

--- Análise Univariada, Bivariada e Multivariada/script-exemplo-analise-univariada.sql
+++ Análise Univariada, Bivariada e Multivariada/script-exemplo-analise-univariada.sql
@@ -19,3 +19,25 @@ ORDER BY
 	media_ocorrencias DESC;
 
 ------------------------------------------------------------------------------------------------------------------ #
+
+-- Isso é uma análise univariada ou multivariada ?
+
+-- Notar que ainda é uma análise univariada, visto que, ainda estamos avaliando UMA VARIÁVEL. Mas como isso possível se temos duas variáveis na análise abaixo ?
+
+-- Isso se dá por conta que são variáveis independentes, ou seja, elas não possuem correlação direta ou cálculo entre ambas as variáveis, ainda não univariadas, ou seja, uma não depende da coexistência da outra.
+
+SELECT
+	 location
+    ,AVG(total_deaths) AS media_ocorrencias
+    ,AVG(new_cases) AS media_novas_ocorrencias
+
+FROM dsa_module_seven.covid_mortes
+
+GROUP BY
+	 location
+     
+ORDER BY
+	media_ocorrencias DESC;
+
+
+------------------------------------------------------------------------------------------------------------------ #
